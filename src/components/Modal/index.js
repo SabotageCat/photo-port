478d83919df9028007a3acd0e3d6b1c45427ae76
Robert D Gonzales <robert.gonzo13@yahoo.com>
2022-06-16 12:27:44 -0500

    created Modal component as child to PhotoList component

---
+++ src/components/Modal/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Modal() {
+
+    return (
+        <div className='modalBackDrop'>
+            <div className='modalContainer'>
+                <h3 className='modalTitle'>Photo Name</h3>
+                <img alt='currentCategory' />
+                <p>
+                    Photo Description
+                </p>
+                <button type='button'>
+                    Close this modal
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default Modal;
